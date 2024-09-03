1ac55d87cdce0ce5dc68b3bd55a95a4812d13e7c
东方月初 <165906706+damizai@users.noreply.github.com>
2024-09-03 15:05:48 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,10 +43,11 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Cloudflaremonitor } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
+import { Money16Regular } from "@v2icons/fluent";
 import siteLinks from "@/assets/siteLinks.json";
 
 const store = mainStore();
@@ -70,6 +71,7 @@ const siteIcon = {
   Book,
   Fire,
   LaptopCode,
+  Cloudflaremonitor,  
 };
 
 // 链接跳转
